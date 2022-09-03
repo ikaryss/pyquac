e5208c5414dca0235060e6128406b4820f637249
Nikolay <nokolay.zh@gmail.com>
2022-09-03 22:32:20 +0300

    finish unit tests

--- tests/test_spectroscopy.py
+++ tests/test_spectroscopy.py
@@ -1,4 +1,5 @@
 import numpy as np
+from typing import Iterable
 from pyquac.datatools import Spectroscopy
 import pytest
 
@@ -89,60 +90,251 @@ def test_iter_setup(x_key, y_key, x_min, x_max, y_min, y_max):
     data = Spectroscopy(x_min=x_min_d, x_max=x_max_d, x_step=x_step_d, y_min=y_min_d, y_max=y_max_d,
                         ny_points=ny_points_d)
     data.iter_setup(x_key=x_key, y_key=y_key, x_min=x_min, x_max=x_max, y_min=y_min, y_max=y_max)
+    
+    # X mask set
     if x_key is not None:
         x_key = data._Spectroscopy__config_closest_values(x_key, data.x_list)
-        x_mask = data.x_1d[np.isin(data.x_1d, x_key)]
+
+        msk = np.isin(data.x_1d, x_key)
+        if (y_min is not None) and (y_max is None):
+                y_min = data._Spectroscopy__config_closest_values(y_min, data.y_list)
+                msk = ((np.isin(data.x_1d, x_key)) & (data.y_1d >= y_min))
+                y_mask = data.y_1d[msk]
+        elif (y_max is not None) and (y_min is None):
+                y_max = data._Spectroscopy__config_closest_values(y_max, data.y_list)
+                msk = (np.isin(data.x_1d, x_key)) & (data.y_1d <= y_max)
+                y_mask = data.y_1d[msk]
+        elif (y_min is not None) and (y_max is not None):
+                y_min = data._Spectroscopy__config_closest_values(y_min, data.y_list)
+                y_max = data._Spectroscopy__config_closest_values(y_max, data.y_list)
+                msk = (np.isin(data.x_1d, x_key)) & (data.y_1d >= y_min) & (data.y_1d <= y_max)
+                y_mask = data.y_1d[msk]
+        elif y_key is not None:
+                y_key = data._Spectroscopy__config_closest_values(y_key, data.y_list)
+                msk = np.isin(data.x_1d, x_key) & np.isin(data.y_1d, y_key)
+                y_mask = data.y_1d[msk]
+        else:
+                y_mask = None
+                pass
+        x_mask = data.x_1d[msk]
+
     elif (x_min is not None) and (x_max is None):
         x_min = data._Spectroscopy__config_closest_values(x_min, data.x_list)
-        x_mask = data.x_1d[data.x_1d >= x_min]
+        msk = data.x_1d >= x_min
+
+        if (y_min is not None) and (y_max is None):
+                y_min = data._Spectroscopy__config_closest_values(y_min, data.y_list)
+                msk = (data.x_1d >= x_min) & (data.y_1d >= y_min)
+                y_mask = data.y_1d[msk]
+        elif (y_max is not None) and (y_min is None):
+                y_max = data._Spectroscopy__config_closest_values(y_max, data.y_list)
+                msk = (data.x_1d >= x_min) & (data.y_1d <= y_max)
+                y_mask = data.y_1d[msk]
+        elif (y_min is not None) and (y_max is not None):
+                y_min = data._Spectroscopy__config_closest_values(y_min, data.y_list)
+                y_max = data._Spectroscopy__config_closest_values(y_max, data.y_list)
+
+                msk = (data.x_1d >= x_min) & (data.y_1d >= y_min) & (data.y_1d <= y_max)
+                y_mask = data.y_1d[msk]
+        elif y_key is not None:
+                y_key = data._Spectroscopy__config_closest_values(y_key, data.y_list)
+                msk = (data.x_1d >= x_min) & np.isin(data.y_1d, y_key)
+                y_mask = data.y_1d[msk]
+        else:
+                y_mask = None
+                pass
+
+        x_mask = data.x_1d[msk]
+
     elif (x_max is not None) and (x_min is None):
         x_max = data._Spectroscopy__config_closest_values(x_max, data.x_list)
-        x_mask = data.x_1d[data.x_1d <= x_max]
+        msk = data.x_1d <= x_max
+
+        if (y_min is not None) and (y_max is None):
+                y_min = data._Spectroscopy__config_closest_values(y_min, data.y_list)
+                msk = (data.x_1d <= x_max) & (data.y_1d >= y_min)
+                y_mask = data.y_1d[msk]
+        elif (y_max is not None) and (y_min is None):
+                y_max = data._Spectroscopy__config_closest_values(y_max, data.y_list)
+                msk = (data.x_1d <= x_max) & (data.y_1d <= y_max)
+                y_mask = data.y_1d[msk]
+        elif (y_min is not None) and (y_max is not None):
+                y_min = data._Spectroscopy__config_closest_values(y_min, data.y_list)
+                y_max = data._Spectroscopy__config_closest_values(y_max, data.y_list)
+                msk = (data.x_1d <= x_max) & (data.y_1d >= y_min) & (data.y_1d <= y_max)
+                y_mask = data.y_1d[msk]
+        elif y_key is not None:
+                y_key = data._Spectroscopy__config_closest_values(y_key, data.y_list)
+                msk = (data.x_1d <= x_max) & np.isin(data.y_1d, y_key)
+                y_mask = data.y_1d[msk]
+        else:
+                y_mask = None
+                pass
+        x_mask = data.x_1d[msk]
+
+
     elif (x_min is not None) and (x_max is not None):
         x_min = data._Spectroscopy__config_closest_values(x_min, data.x_list)
         x_max = data._Spectroscopy__config_closest_values(x_max, data.x_list)
-        x_mask = data.x_1d[(data.x_1d >= x_min) & (data.x_1d <= x_max)]
+        msk = (data.x_1d >= x_min) & (data.x_1d <= x_max)
+
+        if (y_min is not None) and (y_max is None):
+                y_min = data._Spectroscopy__config_closest_values(y_min, data.y_list)
+                msk = (data.x_1d >= x_min) & (data.x_1d <= x_max) & (data.y_1d >= y_min)
+                y_mask = data.y_1d[msk]
+        elif (y_max is not None) and (y_min is None):
+                y_max = data._Spectroscopy__config_closest_values(y_max, data.y_list)
+                msk = (data.x_1d >= x_min) & (data.x_1d <= x_max) & (data.y_1d <= y_max)
+                y_mask = data.y_1d[msk]
+        elif (y_min is not None) and (y_max is not None):
+                y_min = data._Spectroscopy__config_closest_values(y_min, data.y_list)
+                y_max = data._Spectroscopy__config_closest_values(y_max, data.y_list)
+                msk = (data.x_1d >= x_min) & (data.x_1d <= x_max) & (data.y_1d >= y_min) & (data.y_1d <= y_max)
+                y_mask = data.y_1d[msk]
+        elif y_key is not None:
+                y_key = data._Spectroscopy__config_closest_values(y_key, data.y_list)
+                msk = (data.x_1d >= x_min) & (data.x_1d <= x_max) & np.isin(data.y_1d, y_key)
+                y_mask = data.y_1d[msk]
+        else:
+                y_mask = None
+                pass
+        x_mask = data.x_1d[msk]
+
     else:
         x_mask = None
         print('x mask is undefined')
 
+    # Y mask set
     if y_key is not None:
+        print('Y_KEEEEEEY', y_key)
         y_key = data._Spectroscopy__config_closest_values(y_key, data.y_list)
-        y_mask = data.y_1d[np.isin(data.y_1d, y_key)]
+
+        msk = np.isin(data.y_1d, y_key)
+        if (x_min is not None) and (x_max is None):
+                x_min = data._Spectroscopy__config_closest_values(x_min, data.x_list)
+                msk = ((np.isin(data.y_1d, y_key)) & (data.x_1d >= x_min))
+                x_mask = data.x_1d[msk]
+        elif (x_max is not None) and (x_min is None):
+                x_max = data._Spectroscopy__config_closest_values(x_max, data.x_list)
+                msk = (np.isin(data.y_1d, y_key)) & (data.x_1d <= x_max)
+                x_mask = data.x_1d[msk]
+        elif (x_min is not None) and (x_max is not None):
+                x_min = data._Spectroscopy__config_closest_values(x_min, data.x_list)
+                x_max = data._Spectroscopy__config_closest_values(x_max, data.x_list)
+                msk = (np.isin(data.y_1d, y_key)) & (data.x_1d >= x_min) & (data.x_1d <= x_max)
+                x_mask = data.x_1d[msk]
+        elif x_key is not None:
+                x_key = data._Spectroscopy__config_closest_values(x_key, data.x_list)
+                msk = np.isin(data.y_1d, y_key) & np.isin(data.x_1d, x_key)
+                x_mask = data.x_1d[msk]
+        else:
+                x_mask = None
+                pass
+        y_mask = data.y_1d[msk]
+            
     elif (y_min is not None) and (y_max is None):
+
         y_min = data._Spectroscopy__config_closest_values(y_min, data.y_list)
-        y_mask = data.y_1d[data.y_1d >= y_min]
+        msk = data.y_1d >= y_min
+
+        if (x_min is not None) and (x_max is None):
+                x_min = data._Spectroscopy__config_closest_values(x_min, data.x_list)
+                msk = (data.y_1d >= y_min) & (data.x_1d >= x_min)
+                x_mask = data.x_1d[msk]
+        elif (x_max is not None) and (x_min is None):
+                x_max = data._Spectroscopy__config_closest_values(x_max, data.x_list)
+                msk = (data.y_1d >= y_min) & (data.x_1d <= x_max)
+                x_mask = data.x_1d[msk]
+        elif (x_min is not None) and (x_max is not None):
+                x_min = data._Spectroscopy__config_closest_values(x_min, data.x_list)
+                x_max = data._Spectroscopy__config_closest_values(x_max, data.x_list)
+
+                msk = (data.y_1d >= y_min) & (data.x_1d >= x_min) & (data.x_1d <= x_max)
+                x_mask = data.x_1d[msk]
+        elif x_key is not None:
+                x_key = data._Spectroscopy__config_closest_values(x_key, data.x_list)
+                msk = (data.y_1d >= y_min) & np.isin(data.x_1d, x_key)
+                x_mask = data.x_1d[msk]
+        else:
+                x_mask = None
+                pass
+
+        y_mask = data.y_1d[msk]
+
     elif (y_max is not None) and (y_min is None):
         y_max = data._Spectroscopy__config_closest_values(y_max, data.y_list)
-        y_mask = data.y_1d[data.y_1d <= y_max]
+        msk = data.y_1d <= y_max
+
+        if (x_min is not None) and (x_max is None):
+                x_min = data._Spectroscopy__config_closest_values(x_min, data.x_list)
+                msk = (data.y_1d <= y_max) & (data.x_1d >= x_min)
+                x_mask = data.x_1d[msk]
+        elif (x_max is not None) and (x_min is None):
+                x_max = data._Spectroscopy__config_closest_values(x_max, data.x_list)
+                msk = (data.y_1d <= y_max) & (data.x_1d <= x_max)
+                x_mask = data.x_1d[msk]
+        elif (x_min is not None) and (x_max is not None):
+                x_min = data._Spectroscopy__config_closest_values(x_min, data.x_list)
+                x_max = data._Spectroscopy__config_closest_values(x_max, data.x_list)
+                msk = (data.y_1d <= y_max) & (data.x_1d >= x_min) & (data.x_1d <= x_max)
+                x_mask = data.x_1d[msk]
+        elif x_key is not None:
+                x_key = data._Spectroscopy__config_closest_values(x_key, data.x_list)
+                msk = (data.y_1d <= y_max) & np.isin(data.x_1d, x_key)
+                x_mask = data.x_1d[msk]
+        else:
+                x_mask = None
+                pass
+        y_mask = data.y_1d[msk]
+
+
     elif (y_min is not None) and (y_max is not None):
         y_min = data._Spectroscopy__config_closest_values(y_min, data.y_list)
         y_max = data._Spectroscopy__config_closest_values(y_max, data.y_list)
-        y_mask = data.y_1d[(data.y_1d >= y_min) & (data.y_1d <= y_max)]
+        msk = (data.y_1d >= y_min) & (data.y_1d <= y_max)
+
+        if (x_min is not None) and (x_max is None):
+                x_min = data._Spectroscopy__config_closest_values(x_min, data.x_list)
+                msk = (data.y_1d >= y_min) & (data.y_1d <= y_max) & (data.x_1d >= x_min)
+                x_mask = data.x_1d[msk]
+        elif (x_max is not None) and (x_min is None):
+                x_max = data._Spectroscopy__config_closest_values(x_max, data.x_list)
+                msk = (data.y_1d >= y_min) & (data.y_1d <= y_max) & (data.x_1d <= x_max)
+                x_mask = data.x_1d[msk]
+        elif (x_min is not None) and (x_max is not None):
+                x_min = data._Spectroscopy__config_closest_values(x_min, data.x_list)
+                x_max = data._Spectroscopy__config_closest_values(x_max, data.x_list)
+                msk = (data.y_1d >= y_min) & (data.y_1d <= y_max) & (data.x_1d >= x_min) & (data.x_1d <= x_max)
+                x_mask = data.x_1d[msk]
+        elif x_key is not None:
+                x_key = data._Spectroscopy__config_closest_values(x_key, data.x_list)
+                msk = (data.y_1d >= y_min) & (data.y_1d <= y_max) & np.isin(data.x_1d, x_key)
+                x_mask = data.x_1d[msk]
+        else:
+                x_mask = None
+                pass
+        y_mask = data.y_1d[msk]
+
     else:
         y_mask = None
         print('y mask is undefined')
 
-    # print('data.frequency', data.frequency)
-    # print('y_mask', y_mask)
-    #
-    # print('data.load', data.load)
-    # print('x_mask', x_mask)
-
     if (x_mask is not None) and (y_mask is not None):
-        print('FREQ\n', data.frequency)
-        print('YMSK\n', y_mask)
-
-        print('LOAD\n', data.load)
-        print('XMSK\n', x_mask)
-        assert all(data.frequency == y_mask) and all(data.load == x_mask)
+        data_load = np.array(sorted(np.copy(data.load))) if isinstance(data.load, Iterable) else np.copy(data.load)
+        data_frequency = np.array(sorted(np.copy(data.frequency))) if isinstance(data.frequency, Iterable) else np.copy(data.frequency)
+        x_mask = np.array(sorted(x_mask)) if isinstance(x_mask, Iterable) else np.copy(x_mask)
+        y_mask = np.array(sorted(y_mask)) if isinstance(y_mask, Iterable) else np.copy(y_mask)
+        # print(all(data_frequency == y_mask) and all(data_load == x_mask))
+        assert all(data_frequency == y_mask) and all(data_load == x_mask)
 
     elif (x_mask is not None) and (y_mask is None):
-        print('LOAD\n', data.load)
-        print('XMSK\n', x_mask)
-        assert all(data.load == x_mask)
+        data_load = np.array(sorted(np.copy(data.load))) if isinstance(data.load, Iterable) else np.copy(data.load)
+        x_mask = np.array(sorted(x_mask)) if isinstance(x_mask, Iterable) else np.copy(x_mask)
+        # print(all(data_load == x_mask))
+        assert all(data_load == x_mask)
 
     if (x_mask is None) and (y_mask is not None):
-        print('FREQ\n', data.frequency)
-        print('YMSK\n', y_mask)
-        assert all(data.frequency == y_mask)
+        data_frequency = np.array(sorted(np.copy(data.frequency))) if isinstance(data.frequency, Iterable) else np.copy(data.frequency)
+        y_mask = np.array(sorted(y_mask)) if isinstance(y_mask, Iterable) else np.copy(y_mask)
+        # print(all(data_frequency == y_mask))
+        assert all(np.equal(data_frequency, y_mask))
