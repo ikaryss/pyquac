306daf64115f2a8f7744961c397c0fb46f713dd3
Nikolay <nokolay.zh@gmail.com>
2022-11-20 13:10:29 +0300

    add logo to settings update gitignore remove old test script for dash app

--- pyquac/settings.py
+++ pyquac/settings.py
@@ -10,13 +10,16 @@ class Settings(BaseSettings):
 
     # Dash/Plotly
     debug: bool = True
-    css_url: str = r'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'
-    app_name: str = 'FMN lab | Pyquac'
-    app_logo: str = r'/assets/logo_sign.png'
-    app_logo_url: str = r'https://github.com/ikaryss/pyquac/blob/main/images/logo_sign.png?raw=true'
-    app_link: str = r'https://fmn.bmstu.ru/'
-    app_linkedin_url: str = r'https://fmn.bmstu.ru/'
-    app_github_url: str = r'https://github.com/ikaryss/pyquac'
+    css_url: str = r"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    app_name: str = "FMN lab | pyquac"
+    app_logo: str = r"/assets/logo_sign.png"
+    app_logo_url: str = (
+        r"https://github.com/ikaryss/pyquac/blob/main/images/logo_sign.png?raw=true"
+    )
+    app_logo_pyquac: str = r"assets/logo_pyquac.png"
+    app_link: str = r"https://fmn.bmstu.ru/"
+    app_linkedin_url: str = r"https://fmn.bmstu.ru/"
+    app_github_url: str = r"https://github.com/ikaryss/pyquac"
 
     class Config:
         """
@@ -29,5 +32,6 @@ class Settings(BaseSettings):
 
         env_prefix = "SMD_"
         # SMD stands for Stock Market Dashboard
+
 
 settings = Settings()
